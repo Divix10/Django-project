cda1a4ddc6c3884087d92fc23c10a13cba7897f9
David Morales <david@soyasi.es>
2020-09-25 20:41:50 +0200

    Creado modelo de Página

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+# Modelo para las páginas
+class Page(models.Model):
+
+    # Campos de la página
+    title = models.CharField(max_length=50, verbose_name='Título')
+    content = models.TextField(verbose_name='Contenido')
+    slug = models.CharField(unique=True, max_length=150, verbose_name='URL amigable')
+    visible = models.BooleanField(verbose_name='¿Visible?')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creada el')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Actualizada el')
+
+    # Clase meta para configurar parámetros
+    class Meta:
+
+        verbose_name = 'Página'
+        verbose_name_plural = 'Páginas'
+
+    # Método para que imprima el título de la página como un String
+    def __str__(self):
+        return self.title
+    
+
+
