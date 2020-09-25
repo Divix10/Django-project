3f785ba179f5a2d47bc6c7107d787bde74a8b16e
David Morales <david@soyasi.es>
2020-09-25 20:56:47 +0200

    Registrado el modelo de Página para poder gestionarlo en el panel de administración

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Page
 
 # Register your models here.
+
+# Agreamos el modelo para poder gestionarlo desde el panel de admin
+admin.site.register(Page)
