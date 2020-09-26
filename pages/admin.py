8a8c38f33a08fbbf84543e07e9bdf00b4aa10675
David Morales <david@soyasi.es>
2020-09-26 12:07:28 +0200

    Creado el CONTEXT PROCESSOR para poder acceder a las páginas desde cualquier elemento de la app

--- pages/admin.py
+++ pages/admin.py
@@ -5,3 +5,11 @@ from .models import Page
 
 # Agreamos el modelo para poder gestionarlo desde el panel de admin
 admin.site.register(Page)
+
+# Configuración panel de administración
+title = 'Proyecto con Django'
+subtitle = 'Panel del control'
+
+admin.site.site_header = title
+admin.site.site_title = title
+admin.site.index_title = subtitle
---
+++ pages/context_processors.py
@@ -0,0 +1,11 @@
+from pages.models import Page
+
+# Función que selecciona todas las páginas para poder ser usadas en cualquier template
+def get_pages(request):
+
+    # Seleccionamos en la BD los atributos de la página
+    pages = Page.objects.values_list('id', 'title', 'slug')
+
+    return {
+        'pages': pages
+    }
