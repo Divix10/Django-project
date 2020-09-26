4d6e342f31e5db7c9681e0b3ce2acf1a6816821a
David Morales <david@soyasi.es>
2020-09-26 12:24:31 +0200

    Sistema de página y urls para el menú

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Page
 
 # Create your views here.
-def page(request):
+def page(request, slug):
 
-    return
+    # Seleccionamos los datos de la página usando el SLUG que nos llega
+    page = Page.objects.get(slug=slug) # Equivale a SELECT * FROM Page WHERE...
+
+    return render(request, 'pages/page.html', {
+        'page': page
+    })
